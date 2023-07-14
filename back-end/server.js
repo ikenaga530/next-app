fc33aae5685c800c3f0593a204ef27c608b2a172
ikenaga530 <takumi.ne.jp530@gmail.com>
2023-07-14 12:15:27 +0900

    backend 1st done

--- back-end/server.js
+++ back-end/server.js
@@ -1,25 +1,11 @@
+require("dotenv").config();
 const express = require("express");
 const app = express();
-const { PrismaClient } = require("@prisma/client");
+const authRoute = require("./routers/auth");
 const PORT = 8000;
-const bcrypt = require("bcrypt");
 
-const prisma = new PrismaClient();
 app.use(express.json());
-//新規登録ユーザーAPI
-app.post("/api/auth/register", async (req, res) => {
-  const { username, email, password } = req.body;
 
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  const user = await prisma.user.create({
-    data: {
-      username,
-      email,
-      password: hashedPassword,
-    },
-  });
-  return res.json({ user });
-});
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => console.log(`sever is runnning on Port $(PORT)`));
